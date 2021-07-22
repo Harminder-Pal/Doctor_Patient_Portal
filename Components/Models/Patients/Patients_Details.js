b910359f6527641855fdc1fb8e44f9d67c0484e4
Harminder-Pal <85278472+Harminder-Pal@users.noreply.github.com>
2021-07-22 11:09:23 +0530

    Add files via upload

--- Components/Models/Patients/Patients_Details.js
+++ Components/Models/Patients/Patients_Details.js
@@ -1,1 +1,25 @@
+const  Sequelize  = require("sequelize");
 
+const sequelize = require('../util/database');
+
+const Patients_Details = sequelize.define('Patients_Details',{
+    Patient_Id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    Name:{
+        type:Sequelize.STRING,
+        allowNull:false,
+    },
+    Phone_Number:{
+        type:Sequelize.INTEGER,
+        allowNull:false
+    },
+    Email_Id:{
+        type:Sequelize.STRING,
+        allowNull:false
+    }
+})
+
+module.exports = Patients_Details;
